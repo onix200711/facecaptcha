4f36edf37aff2e31c7f469f0e1e98f624ac2ce9d
ZhasBas <159027094+ZhasBas@users.noreply.github.com>
2024-07-26 15:18:38 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://facecaptcha.me",
+]
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
